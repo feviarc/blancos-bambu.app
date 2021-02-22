268c5314fbbff98f5897f4c617f697d7502f770d
Felipe Víctor <felipe@oitsys.com>
2021-02-21 19:05:38 -0600

    VerifyEmail component main logic implemented

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service'; 
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.scss']
 })
+
 export class VerifyEmailComponent implements OnInit {
 
-  constructor() { }
+  alertMessage: string;
+  resendButtonDisabled: boolean;
 
-  ngOnInit(): void {
+
+  constructor(public authService: AuthService, public router: Router) { 
+    this.alertMessage = '';
+    this.resendButtonDisabled = false;
+  }
+
+
+  ngOnInit(): void { }
+
+
+  goToLogin() {
+    this.router.navigate(['sign-in']);
+  }
+
+
+  sendVerificationEmail() {
+    this.authService.sendVerificationMail()
+    .then(
+      () => {
+        this.router.navigate(['sign-in']);
+      }
+    )
+    .catch(
+      error => {
+        this.alertMessage = error.message;
+        this.resendButtonDisabled = true;
+        setTimeout(()=>{this.alertMessage=''},5000);
+      }
+    );
   }
 
 }
