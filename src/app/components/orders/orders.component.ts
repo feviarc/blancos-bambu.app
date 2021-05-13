90500b5147436810d1a7ad02f3800f662d159315
Felipe Víctor <felipe@oitsys.com>
2021-05-12 22:24:53 -0500

    Sort feature was added to orders management table

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { FirebaseCRUDService } from '../../shared/services/firebase-crud.service';
 
 
@@ -10,24 +12,46 @@ import { FirebaseCRUDService } from '../../shared/services/firebase-crud.service
 
 export class OrdersComponent implements OnInit {
 
+  dataSource: any;
   displayedColumns: string[];
   isLoadingData: boolean;
-  orders: any;
+  @ViewChild(MatSort) sort: any;
 
 
   constructor(private crudService: FirebaseCRUDService) {
     this.displayedColumns = [
-      'productAmount',
+      'registerDate',
+      'resellerDisplayName',
       'productName',
       'productBrand',
-      'reseller',
-      'registerDate',
-      'crud-icons'
+      'amount',
+      'isInStore',
+      'icons'
     ];
     this.isLoadingData = true;
     this.crudService.getActiveOrders().subscribe(
       documents => {
-        this.orders = documents;
+        const orders = documents.map(
+          (order) => {
+            return {
+              orderID: order.id,
+              resellerID: order.reseller.id,
+              resellerDisplayName: order.reseller.displayName,
+              productID: order.product.id,
+              productBrandCode: order.product.brandCode,
+              productName: order.product.name,
+              productBrand: order.product.brand,
+              isDelivered: order.status.isDelivered,
+              registerDate: order.status.registerDate,
+              deliveryDate: order.status.deliveryDate,
+              amount: order.amount,
+              isInStore: order.isInStore,
+              comments: order.coments
+            };
+          }
+        );
+        this.dataSource = new MatTableDataSource(orders);
+        this.dataSource.sort = this.sort;
         this.isLoadingData = false;
       }
     );
