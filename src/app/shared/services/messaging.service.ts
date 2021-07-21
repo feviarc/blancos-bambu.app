5fec8801702b0a8e6df95316094dda58c2ba2b1e
Felipe Víctor <felipe@oitsys.com>
2021-07-21 14:42:24 -0500

    The request messaging permission to get a Token was implemented

--- src/app/shared/services/messaging.service.ts
+++ src/app/shared/services/messaging.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AngularFireMessaging } from '@angular/fire/messaging';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { app } from '../../../environments/environment.app';
+
+interface Token {
+  token: string | null;
+}
 
 
 @Injectable({
@@ -7,6 +14,35 @@ import { Injectable } from '@angular/core';
 
 export class MessagingService {
 
-  constructor() { }
+  private tokensRef: AngularFirestoreCollection<Token>;
+
+
+  constructor(
+    private db: AngularFirestore,
+    private messaging: AngularFireMessaging
+  ) {
+    this.tokensRef = this.db.collection<Token>(app.db.path.tokens);
+  }
+
+
+  requestMessagingPermission() {
+    this.messaging.requestToken.subscribe(
+      token => {
+        if (token) {
+          console.log(token);
+          this.tokensRef.add({token});
+        }
+      }
+    );
+  }
+
+
+  listeningNotifications() {
+    this.messaging.messages.subscribe(
+      message => {
+        console.log('message: ', message);
+      }
+    );
+  }
 
 }
