bd05a4b78ae117f12386c0c5584e9fefcc78bf55
Felipe Víctor <felipe@oitsys.com>
2022-01-14 14:40:53 -0600

    The DeliveredOrdersList component was created

---
+++ src/app/components/orders/delivered-orders-list/delivered-orders-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivered-orders-list',
+  templateUrl: './delivered-orders-list.component.html',
+  styleUrls: ['./delivered-orders-list.component.scss']
+})
+
+export class DeliveredOrdersListComponent {
+
+  constructor() { }
+
+}
