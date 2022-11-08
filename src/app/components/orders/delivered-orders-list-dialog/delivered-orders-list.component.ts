ebd4b518604fb6f4b915bb61fab128738974de8d
Felipe Víctor <felipe@oitsys.com>
2022-11-08 11:14:38 -0600

    The fields in the delivered orders table were set up

--- src/app/components/orders/delivered-orders-list-dialog/delivered-orders-list.component.ts
+++ src/app/components/orders/delivered-orders-list-dialog/delivered-orders-list.component.ts
@@ -1,63 +1,63 @@
 import { Component } from '@angular/core';
 
-export interface PeriodicElement {
+export interface DeliveredOrder {
+  amount: number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  reseller: string;
+  deliveryDate: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: DeliveredOrder[] = [
+  {amount: 1, name: 'Hydrogen', reseller: '1.0079', deliveryDate: 'H'},
+  {amount: 2, name: 'Helium', reseller: '4.0026', deliveryDate: 'He'},
+  {amount: 3, name: 'Lithium', reseller: '6.941', deliveryDate: 'Li'},
+  {amount: 4, name: 'Beryllium', reseller: '9.0122', deliveryDate: 'Be'},
+  {amount: 5, name: 'Boron', reseller: '10.811', deliveryDate: 'B'},
+  {amount: 6, name: 'Carbon', reseller: '12.0107', deliveryDate: 'C'},
+  {amount: 7, name: 'Nitrogen', reseller: '14.0067', deliveryDate: 'N'},
+  {amount: 8, name: 'Oxygen', reseller: '15.9994', deliveryDate: 'O'},
+  {amount: 9, name: 'Fluorine', reseller: '18.9984', deliveryDate: 'F'},
+  {amount: 10, name: 'Neon', reseller: '20.1797', deliveryDate: 'Ne'},
+  {amount: 1, name: 'Hydrogen', reseller: '1.0079', deliveryDate: 'H'},
+  {amount: 2, name: 'Helium', reseller: '4.0026', deliveryDate: 'He'},
+  {amount: 3, name: 'Lithium', reseller: '6.941', deliveryDate: 'Li'},
+  {amount: 4, name: 'Beryllium', reseller: '9.0122', deliveryDate: 'Be'},
+  {amount: 5, name: 'Boron', reseller: '10.811', deliveryDate: 'B'},
+  {amount: 6, name: 'Carbon', reseller: '12.0107', deliveryDate: 'C'},
+  {amount: 7, name: 'Nitrogen', reseller: '14.0067', deliveryDate: 'N'},
+  {amount: 8, name: 'Oxygen', reseller: '15.9994', deliveryDate: 'O'},
+  {amount: 9, name: 'Fluorine', reseller: '18.9984', deliveryDate: 'F'},
+  {amount: 10, name: 'Neon', reseller: '20.1797', deliveryDate: 'Ne'},
+  {amount: 1, name: 'Hydrogen', reseller: '1.0079', deliveryDate: 'H'},
+  {amount: 2, name: 'Helium', reseller: '4.0026', deliveryDate: 'He'},
+  {amount: 3, name: 'Lithium', reseller: '6.941', deliveryDate: 'Li'},
+  {amount: 4, name: 'Beryllium', reseller: '9.0122', deliveryDate: 'Be'},
+  {amount: 5, name: 'Boron', reseller: '10.811', deliveryDate: 'B'},
+  {amount: 6, name: 'Carbon', reseller: '12.0107', deliveryDate: 'C'},
+  {amount: 7, name: 'Nitrogen', reseller: '14.0067', deliveryDate: 'N'},
+  {amount: 8, name: 'Oxygen', reseller: '15.9994', deliveryDate: 'O'},
+  {amount: 9, name: 'Fluorine', reseller: '18.9984', deliveryDate: 'F'},
+  {amount: 10, name: 'Neon', reseller: '20.1797', deliveryDate: 'Ne'},
+  {amount: 1, name: 'Hydrogen', reseller: '1.0079', deliveryDate: 'H'},
+  {amount: 2, name: 'Helium', reseller: '4.0026', deliveryDate: 'He'},
+  {amount: 3, name: 'Lithium', reseller: '6.941', deliveryDate: 'Li'},
+  {amount: 4, name: 'Beryllium', reseller: '9.0122', deliveryDate: 'Be'},
+  {amount: 5, name: 'Boron', reseller: '10.811', deliveryDate: 'B'},
+  {amount: 6, name: 'Carbon', reseller: '12.0107', deliveryDate: 'C'},
+  {amount: 7, name: 'Nitrogen', reseller: '14.0067', deliveryDate: 'N'},
+  {amount: 8, name: 'Oxygen', reseller: '15.9994', deliveryDate: 'O'},
+  {amount: 9, name: 'Fluorine', reseller: '18.9984', deliveryDate: 'F'},
+  {amount: 10, name: 'Neon', reseller: '20.1797', deliveryDate: 'Ne'},
+  {amount: 1, name: 'Hydrogen', reseller: '1.0079', deliveryDate: 'H'},
+  {amount: 2, name: 'Helium', reseller: '4.0026', deliveryDate: 'He'},
+  {amount: 3, name: 'Lithium', reseller: '6.941', deliveryDate: 'Li'},
+  {amount: 4, name: 'Beryllium', reseller: '9.0122', deliveryDate: 'Be'},
+  {amount: 5, name: 'Boron', reseller: '10.811', deliveryDate: 'B'},
+  {amount: 6, name: 'Carbon', reseller: '12.0107', deliveryDate: 'C'},
+  {amount: 7, name: 'Nitrogen', reseller: '14.0067', deliveryDate: 'N'},
+  {amount: 8, name: 'Oxygen', reseller: '15.9994', deliveryDate: 'O'},
+  {amount: 9, name: 'Fluorine', reseller: '18.9984', deliveryDate: 'F'},
+  {amount: 10, name: 'Neon', reseller: '20.1797', deliveryDate: 'Ne'},
 ];
 
 @Component({
@@ -68,7 +68,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class DeliveredOrdersListComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['amount', 'name', 'reseller', 'deliveryDate'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
