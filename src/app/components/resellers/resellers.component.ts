261ebe56a59f5f9db00a0928187b16868936e740
Felipe Víctor <felipe@oitsys.com>
2022-12-03 21:15:40 -0600

    A MatTable was added to the resellers page

--- src/app/components/resellers/resellers.component.ts
+++ src/app/components/resellers/resellers.component.ts
@@ -9,11 +9,32 @@ import { Component } from '@angular/core';
 
 export class ResellersComponent {
 
-  constructor() { }
+  isLoadingData: boolean;
+  dataSource: any;
+  tableColumns: string[];
+
+  constructor() {
+    this.isLoadingData = true;
+    this.tableColumns = [
+      'firstName',
+      'lastName',
+      'mobilePhone',
+      'crudIcons'
+    ];
+    this.dataSource = [
+      {firstName: 'Felipe de Jesús', lastNameF: 'Víctor', lastNameM: 'Arceo', mobilePhone: '3531003631'},
+      {firstName: 'Ivett', lastNameF: 'López', lastNameM: 'Gutiérrez', mobilePhone: '3531008331'},
+    ]
+  }
 
 
   applyFilter(event: Event) {
     console.log(event);
+  }
+
+
+  openAddResellerSheet(reseller: any) {
+    console.log(reseller);
   }
 
 }
