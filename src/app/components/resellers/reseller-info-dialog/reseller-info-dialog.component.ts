e7f60b42a9d6f799808604f5719eda54bb2541d8
Felipe Víctor <felipe@oitsys.com>
2023-01-09 11:22:37 -0600

    The active orders tab was implemented in the reseller info dialog

--- src/app/components/resellers/reseller-info-dialog/reseller-info-dialog.component.ts
+++ src/app/components/resellers/reseller-info-dialog/reseller-info-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FirebaseCRUDService } from 'src/app/shared/services/firebase-crud.service';
+
 
 @Component({
   selector: 'app-reseller-info-dialog',
@@ -9,10 +11,47 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export class ResellerInfoDialogComponent {
 
+  activeOrdersDataSource: any;
+  deliveredOrdersDataSource: any;
+  displayedColumns: string[] = ['amount', 'name', 'date'];
+
+
   constructor(
-    @Inject(MAT_DIALOG_DATA) public reseller: any
+    @Inject(MAT_DIALOG_DATA) public reseller: any,
+    public crudService: FirebaseCRUDService
   ) {
-    console.log(reseller)
+    crudService.getDeliveredOrdersByReseller(reseller.id).subscribe(
+      documents => {
+        this.deliveredOrdersDataSource = documents.map(
+          order => this.changeOrderFormat(order, 'DELIVERY_DATE')
+        );
+      }
+    );
+    crudService.getActiveOrdersByReseller(reseller.id).subscribe(
+      documents => {
+        this.activeOrdersDataSource = documents.map(
+          order => this.changeOrderFormat(order, 'REGISTER_DATE')
+        );
+      }
+    );
+  }
+
+
+  private changeOrderFormat(order: any, statusDate: string) {
+    let formatedOrder =
+      {
+        amount: order.amount,
+        name: order.product.name,
+        date: null
+      };
+
+    if(statusDate === 'DELIVERY_DATE') {
+      formatedOrder.date = order.status.deliveryDate;
+    } else {
+      formatedOrder.date = order.status.registerDate;
+    }
+
+    return formatedOrder;
   }
 
 }
